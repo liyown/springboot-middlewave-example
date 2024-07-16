f168250e9f70d466b97c9699ca48da8bbb8bf78c
liuyaowen <liuyaowen_smile@126.com>
2024-07-16 21:18:21 +0800

    feat: 初始化 Spring Boot Elasticsearch 项目

---
+++ springboot-elastic-search/src/main/java/com/lyw/springbootelasticsearch/SpringbootElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.lyw.springbootelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootElasticSearchApplication.class, args);
+    }
+
+}
---
+++ springboot-elastic-search/src/test/java/com/lyw/springbootelasticsearch/SpringbootElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyw.springbootelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
